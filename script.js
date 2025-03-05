76b65a9bf44faa6c1563e181a7c0428e64e3d6b4
lwal1s <laurin_wallenstein@mis-munich.de>
2025-03-05 10:24:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function SportsFunction() {
+    document.getElementById("div").textContent =
+        "I enjoy climbing, hiking, skiing/freeriding/skitouring and many other mountain sports. I currently have climbed up to 5c and have done 5b+ onsight. I also enjoy reading fiction and nonfiction (in particular the blog A Collection of Unmitigated Pedantry) and enjoy playing the games TMNF (Trackmania Nations Forever) and Trackmania 2020.";
+}
+
+function TheatreFunction() {
+    document.getElementById("div").textContent =
+        "I have participated in many school plays and also play a TTRPG known as Dungeons and Dragons in my free time, though I wish my DM would switch to PF2e, because it is better.";
+}
